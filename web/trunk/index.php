607b442c85c2aeba45244bfe0669cf1e1dfe7671
helix <helix@c75024a7-4733-0410-87ae-8b2da5a052fe>
2008-11-28 12:37:12 +0000

    Initial php release

---
+++ web/trunk/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en_US" xml:lang="en_US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Language" content="en" />
+	<meta name="GENERATOR" content="PHPEclipse 1.2.0" />
+	<title>Test Page</title>
+</head>
+<body>
+<p>Text Paragraph 1</p>
+<p>Text Paragraph 2</p>
+</body>
+</html>
